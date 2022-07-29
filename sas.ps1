6b52c7cf3f76f2851748d375d31de141b5c40b54
TopazRN <topazsorowako2@gmail.com>
2022-07-29 08:32:36 +0700

    List Registered Users

--- sas.ps1
+++ sas.ps1
@@ -2,7 +2,17 @@
 $current_user_reg = "Registry::HKEY_CURRENT_USER\SOFTWARE\Valve\Steam"
 $current_user = Get-ItemProperty -Path $current_user_reg | Select-Object -ExpandProperty AutoLoginUser
 
-Set-ItemProperty -Path $current_user_reg -Name AutoLoginUser -Value topazsoroako
+# List Registered Users
+$steam_path_reg = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam"
+$steam_path = Get-ItemProperty -Path $steam_path_reg | Select-Object -ExpandProperty InstallPath
+Write-Output "Account list:"
+"    " + $current_user + " [Active]"
+Get-Content $steam_path"\config\loginusers.vdf" | Select-String AccountName | ForEach-Object {
+  $test = $_.ToString().Replace('"AccountName"', '').Trim().Trim('"')
+  if ($test -ne $current_user) {
+    "    " + $test
+  }
+}
 
 $current_user = Get-ItemProperty -Path $current_user_reg | Select-Object -ExpandProperty AutoLoginUser
 Write-Output $current_user
