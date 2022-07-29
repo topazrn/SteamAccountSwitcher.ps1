b21f4d583de9141eb14c980fc546a3c7c67290a1
TopazRN <topazsorowako2@gmail.com>
2022-07-29 08:32:12 +0700

    Get Current User

--- sas.ps1
+++ sas.ps1
@@ -1,11 +1,6 @@
-# $steam_path_reg = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Valve\Steam"
-# $steam_path = Get-ItemProperty -Path $steam_path_reg | Select-Object -ExpandProperty InstallPath
-
-# Get-Content $steam_path"\config\loginusers.vdf"
-
+# Get Current User
 $current_user_reg = "Registry::HKEY_CURRENT_USER\SOFTWARE\Valve\Steam"
 $current_user = Get-ItemProperty -Path $current_user_reg | Select-Object -ExpandProperty AutoLoginUser
-Write-Output $current_user
 
 Set-ItemProperty -Path $current_user_reg -Name AutoLoginUser -Value topazsoroako
 
